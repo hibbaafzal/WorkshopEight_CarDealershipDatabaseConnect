555abd71f40fc1934a2adf72768f79b5dfa040f9
hibbaafzal <166542360+hibbaafzal@users.noreply.github.com>
2024-06-14 18:02:12 -0400

    vehicle interface added

--- src/main/java/com/ps/dao/interfaces/VehicleInt.java
+++ src/main/java/com/ps/dao/interfaces/VehicleInt.java
@@ -2,6 +2,7 @@ package com.ps.dao.interfaces;
 
 import com.ps.models.Vehicle;
 
+
 import java.util.List;
 
 public interface VehicleInt {
@@ -13,11 +14,9 @@ public interface VehicleInt {
 
     List<Vehicle> getVehiclesByYearRange(int startYear, int endYear);
 
+
     List<Vehicle> getVehiclesByColor(String color);
 
- //   List<Vehicle> getVehiclesByMileageRange(int minMileage, int maxMileage);
-
-  //  List<Vehicle> getVehiclesByType(String type);
 
     int createVehicle(Vehicle vehicle);
 
